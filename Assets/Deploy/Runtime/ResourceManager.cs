4326c587b56cae9ff835f67ea3841988ff4f0d2f
causeless7t <145838385+causeless8t@users.noreply.github.com>
2026-09-16 18:17:15 +0900

    refactor: remove Addressables integration

--- Assets/Deploy/Runtime/ResourceManager.cs
+++ Assets/Deploy/Runtime/ResourceManager.cs
@@ -6,28 +6,17 @@ using System.Threading;
 using Causeless3t.Core;
 using UnityEngine;
 using Cysharp.Threading.Tasks;
-#if USE_ADDRESSABLES
-using UnityEngine.AddressableAssets;
-using UnityEngine.AddressableAssets.ResourceLocators;
-using UnityEngine.ResourceManagement.AsyncOperations;
-using UnityEngine.ResourceManagement.ResourceLocations;
-#else
 using Causeless3t.Security;
 using Causeless3t.AssetBundle;
 using UnityEngine.Networking;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
-#endif
 
 namespace Causeless3t
 { 
     public sealed class ResourceManager : Singleton<ResourceManager>
     {
-        // private CheckDownloadUi _checkDownloadUi;
-#if USE_ADDRESSABLES
-        private IResourceLocator _catalogHandler;
-#else
         private static readonly string BundleRootPath = Path.Combine(Application.persistentDataPath, "contents");
         
     #if UNITY_EDITOR
@@ -49,7 +38,6 @@ namespace Causeless3t
         }
 
         private readonly Dictionary<string, AssetBundleRef> _cachedBundles = new();
-#endif
 
         private string _remoteURL;
         public void SetRemoteURL(string url) => _remoteURL = url;
@@ -62,17 +50,12 @@ namespace Causeless3t
         {
             IsInitialized = false;
 #if !UNITY_EDITOR
-    #if USE_ADDRESSABLES
-            await Addressables.InitializeAsync(true);
-            Addressables.ClearResourceLocators();
-    #else
             var infoFileText = await File.ReadAllTextAsync(StreamingInfoFilePath);
             var streamingInfoList = JsonUtility.FromJson<ContentsInfoList>(infoFileText);
             if (!Application.version.Equals(streamingInfoList.AppVersion))
                 ClearBundleFiles();
             if (!File.Exists(PersistentInfoFilePath))
                 File.Copy(StreamingInfoFilePath, PersistentInfoFilePath);
-    #endif
 #endif
             IsInitialized = true;
         }
@@ -81,14 +64,12 @@ namespace Causeless3t
 
         public bool IsDownloading { get; private set; }
         
-#if !USE_ADDRESSABLES
         private void ClearBundleFiles()
         {
             var files = Directory.GetFiles(BundleRootPath);
             foreach (var file in files)
                 File.Delete(file);
         }
-#endif
 
         public async UniTask CheckUpdateAsync(Action<float> onProgress = null)
         {
@@ -96,10 +77,8 @@ namespace Causeless3t
             await UniTask.WaitUntil(() => IsInitialized);
 #if !UNITY_EDITOR
             await DownloadUpdatableFiles(onProgress);
-    #if !USE_ADDRESSABLES
             var infoFileText = await File.ReadAllTextAsync(PersistentInfoFilePath);
             _contentsInfoList = JsonUtility.FromJson<ContentsInfoList>(infoFileText);
-    #endif
 #endif
             IsDownloading = false;
             await UniTask.CompletedTask;
@@ -107,59 +86,6 @@ namespace Causeless3t
 
         private async UniTask DownloadUpdatableFiles(Action<float> onProgress = null)
         {
-#if USE_ADDRESSABLES
-            try
-            {
-                _catalogHandler =
-                    await Addressables.LoadContentCatalogAsync($"{_remoteURL}/catalog_1.json", true);
-                var downloadSize = await Addressables.GetDownloadSizeAsync(_catalogHandler.Keys);
-                if (downloadSize > 0)
-                {
-                    Debug.Log($"Found CDN Downloadable Files {_catalogHandler.Keys.Count()}");
-                    _checkDownloadUi =
-                        UiManager.Instance.GetUI<CheckDownloadUi>(UiManager.eSystemUIType.CheckDownloadUI);
-                    _checkDownloadUi.Open();
-                    _checkDownloadUi.SetDownloadText(downloadSize);
-                    await UniTask.WaitUntil(() => !_checkDownloadUi.IsActive);
-                    if (!CheckDownloadUi.IsApproveDownload) return;
-
-                    var downloadAsync =
-                        Addressables.DownloadDependenciesAsync(_catalogHandler.Keys, Addressables.MergeMode.Union,
-                            true);
-                    while (!downloadAsync.IsDone)
-                    {
-                        if (downloadAsync.Status == AsyncOperationStatus.Failed ||
-                            Application.internetReachability == NetworkReachability.NotReachable)
-                        {
-                            Debug.LogError("CDN download exist error");
-                            throw new Exception("Network is not reachable");
-                        }
-
-                        onProgress?.Invoke(downloadAsync.GetDownloadStatus().Percent);
-                        await UniTask.Yield();
-                    }
-
-                    FirebaseManager.Instance.LogEvent("CDN_Complete");
-                }
-                else
-                    Debug.Log("Not found CDN Downloadable Files");
-            }
-            catch (Exception e)
-            {
-                Debug.LogError(e);
-                CommonPopupFactory.CreateErrorPopup(
-                    LocalizationManager.GetLocalizedString("UI_Common_Error"), 
-                    LocalizationManager.GetLocalizedString("UI_CDNNetwork_Error_Changed"),
-                    onClose: _ =>
-                    {
-    #if UNITY_EDITOR
-                        UnityEditor.EditorApplication.isPlaying = false;
-    #else
-                        Application.Quit();
-    #endif
-                    });
-            }
-#else
             // 1. compare with remote fileinfo.dat and local
             var infoFileText = await File.ReadAllTextAsync(PersistentInfoFilePath);
             var localInfoList = JsonUtility.FromJson<ContentsInfoList>(infoFileText);
@@ -189,17 +115,11 @@ namespace Causeless3t
             var modifiedInfoList = CompareFileInfoList(localInfoList, remoteInfoList);
             if (modifiedInfoList.FileInfos.Count == 0) return;
             
-            // 3. Ask to user proceed to download CDN Files
+            // 3. Log the required download size.
             long downloadSize = 0;
             modifiedInfoList.FileInfos.ForEach(info => downloadSize += info.Size);
             
             Debug.Log($"Found CDN Downloadable Files {modifiedInfoList.FileInfos.Count}, Size {downloadSize/(1024*1024)}MB");
-            // _checkDownloadUi =
-            //     UiManager.Instance.GetUI<CheckDownloadUi>(UIManager.eSystemUIType.CheckDownloadUI);
-            // _checkDownloadUi.Open();
-            // _checkDownloadUi.SetDownloadText(downloadSize);
-            // await UniTask.WaitUntil(() => !_checkDownloadUi.IsActive);
-            // if (!CheckDownloadUi.IsApproveDownload) return;
 
             // 4. Remove/Download CDN Files
             List<string> removedKeys = modifiedInfoList.GetRemovableFiles();
@@ -263,71 +183,11 @@ namespace Causeless3t
                 }
                 await UniTask.WhenAll(tasks); // 모든 다운로드 완료 대기
             }
-            // FirebaseManager.Instance.LogEvent("CDN_Complete");
             
             // 5. change local fileinfo.dat to remote
             await File.WriteAllTextAsync(PersistentInfoFilePath, JsonUtility.ToJson(remoteInfoList));
-#endif
         }
         
-#if USE_ADDRESSABLES
-        public async UniTask<Dictionary<string, T>> LoadAssetByLabelsAsync<T>(IEnumerable<string> labels) where T : class
-        {
-            Dictionary<string, T> result = new();
-            var loadResourceLocationsHandle = Addressables.LoadResourceLocationsAsync(labels, Addressables.MergeMode.Union, typeof(T));
-            await loadResourceLocationsHandle;
-
-            //start each location loading
-            List<AsyncOperationHandle> opList = new();
-
-            int completedCount = 0;
-            foreach (IResourceLocation location in loadResourceLocationsHandle.Result)
-            {   
-                // CreateGenericGroupOperation() 을 사용하여 한번에 모든 처리를 기다리기 위하여 await를 사용하지 않는다.
-                AsyncOperationHandle<T> loadAssetHandle = Addressables.LoadAssetAsync<T>(location);
-                loadAssetHandle.Completed += obj =>
-                {
-                    result.TryAdd(location.PrimaryKey, obj.Result);
-                    completedCount++;
-                };
-                opList.Add(loadAssetHandle);
-            }
-
-            //create a GroupOperation to wait on all the above loads at once.
-            var groupOp = Addressables.ResourceManager.CreateGenericGroupOperation(opList);
-            await groupOp;
-            await UniTask.WaitUntil(() => opList.Count == completedCount);
-
-            Addressables.Release(loadResourceLocationsHandle);
-            return result;
-        }
-
-        /// <summary>
-        /// Addressable Path로 리소스를 Load하는 함수. 함께 넘어온 Handle(Item2)을 꼭 Addressable.Release 해줘야 한다.
-        /// </summary>
-        /// <param name="path">Addressable Path</param>
-        /// <typeparam name="T">Load할 형태</typeparam>
-        /// <returns>로드 된 어셋과 핸들을 담은 튜플</returns>
-        public async UniTask<(T, AsyncOperationHandle<T>)> LoadAssetByPathAsync<T>(string path) where T : class
-        {
-            AsyncOperationHandle<T> loadAssetHandle = Addressables.LoadAssetAsync<T>(path);
-            var result = await loadAssetHandle;
-            return (result, loadAssetHandle);
-        }
-        
-        /// <summary>
-        /// GameObject를 Load하여 Instantiate까지 해주는 함수. Addressable.Release 해줄 필요는 없다.
-        /// </summary>
-        /// <param name="path">Addressable Path</param>
-        /// <param name="parent">Instantiate될 부모 Transform</param>
-        /// <param name="instantiateWorldSpace"></param>
-        /// <returns>Instantiate된 객체 GameObject</returns>
-        public async UniTask<GameObject> InstantiateGameObjectByPathAsync(string path, Transform parent = null, bool instantiateWorldSpace = false)
-        {
-            AsyncOperationHandle<GameObject> loadAssetHandle = Addressables.InstantiateAsync(path, parent, instantiateWorldSpace);
-            return await loadAssetHandle;
-        }
-#else
         private ContentsInfoList CompareFileInfoList(ContentsInfoList localList, ContentsInfoList renewalList)
         {
             ContentsInfoList retVal = new ContentsInfoList();
@@ -446,6 +306,5 @@ namespace Causeless3t
                 await pair.Value.Bundle.UnloadAsync(true);
             _cachedBundles.Clear();
         }
-#endif
     }
 }
