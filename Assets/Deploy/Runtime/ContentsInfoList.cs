30c4b631e96cc67b2363a4905ac0275762e2f52f
causeless7t <145838385+causeless8t@users.noreply.github.com>
2026-09-16 21:28:58 +0900

    refactor: clarify contents manifest comparison

--- Assets/Deploy/Runtime/ContentsInfoList.cs
+++ Assets/Deploy/Runtime/ContentsInfoList.cs
@@ -1,81 +1,115 @@
 using System;
-using UnityEngine;
-using System.IO;
 using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
 
 namespace Causeless3t.AssetBundle
 {
-	[Serializable]
-	public class ContentsInfo : IComparable<ContentsInfo>
-	{
-		public string Label;
-		public string Path;
-		public string Hash;
-		public long Size;
+    [Serializable]
+    public sealed class ContentsInfo
+    {
+        public string Label;
+        public string Path;
+        public string Hash;
+        public long Size;
 
-		public int CompareTo(ContentsInfo other)
-		{
-			if (other != null && Path.Equals(other.Path))
-			{
-				if (Size != other.Size)
-					return 1;
-				return Hash.Equals(other.Hash) ? 0 : 1;
-			}
-			return -1;
-		}
-	}
-	
-	[Serializable]
-	public class ContentsInfoList
-	{
-		public List<ContentsInfo> FileInfos = new();
-		public int Platform;
-		public string AppVersion;
-		public int Revision;
-		public int FileCount;
-		private List<ContentsInfo> _removableFiles = new();
+        public bool HasSameContent(ContentsInfo other)
+        {
+            return other != null &&
+                   string.Equals(Path, other.Path, StringComparison.Ordinal) &&
+                   Size == other.Size &&
+                   string.Equals(Hash, other.Hash, StringComparison.Ordinal);
+        }
+    }
 
-		public ContentsInfoList() { }
-		
-		public string ToJSONString() => JsonUtility.ToJson(this);
-		
-		public void AddRemovableFile(ContentsInfo fileInfo) => _removableFiles.Add(fileInfo);
-		public List<string> GetRemovableFiles() => _removableFiles.ConvertAll(x => x.Path);
-		
-		public static ContentsInfoList GetContentsInfoListFromFiles(
-			string contentsRootDirPath,
-			Dictionary<string, string> bundleLabelDic,
-			string filteredExtension)
-		{
-			ContentsInfoList retVal = new ContentsInfoList();
-			DirectoryInfo dirInfo = new DirectoryInfo(contentsRootDirPath);
-			FileInfo [] files = dirInfo.GetFiles($"*.{filteredExtension}");
-        
-			foreach (FileInfo file in files)
-			{
-				string fileName = file.Name;
-				long fileSize = file.Length;
-				string crc = AssetBundleUtil.GetFileHash(file.FullName);
-				var key = Path.GetFileNameWithoutExtension(fileName).Replace('~', '/');
-				string label = string.Empty;
-				foreach (KeyValuePair<string, string> pair in bundleLabelDic)
-				{
-					if (pair.Key.EndsWith(key))
-					{
-						label = pair.Value;
-						break;
-					}
-				}
-				
-				retVal.FileInfos.Add(new ContentsInfo()
-				{
-					Label = label,
-					Path = fileName,
-					Hash = crc,
-					Size = fileSize
-				});
-			}
-			return retVal;
-		}
-	}
+    [Serializable]
+    public sealed class ContentsInfoList
+    {
+        public List<ContentsInfo> FileInfos = new();
+        public int Platform;
+        public string AppVersion;
+        public int Revision;
+        public int FileCount;
+
+        [NonSerialized]
+        private List<ContentsInfo> _removableFiles = new();
+
+        public string ToJSONString()
+        {
+            return JsonUtility.ToJson(this);
+        }
+
+        internal void AddRemovableFile(ContentsInfo fileInfo)
+        {
+            if (fileInfo == null)
+                return;
+
+            _removableFiles ??= new List<ContentsInfo>();
+            _removableFiles.Add(fileInfo);
+        }
+
+        internal IReadOnlyList<string> GetRemovableFiles()
+        {
+            if (_removableFiles == null || _removableFiles.Count == 0)
+                return Array.Empty<string>();
+
+            return _removableFiles.ConvertAll(info => info.Path);
+        }
+
+        public static ContentsInfoList GetContentsInfoListFromFiles(
+            string contentsRootDirectoryPath,
+            Dictionary<string, string> bundleLabels,
+            string filteredExtension)
+        {
+            if (string.IsNullOrWhiteSpace(contentsRootDirectoryPath))
+            {
+                throw new ArgumentException(
+                    "Contents root directory path cannot be empty.",
+                    nameof(contentsRootDirectoryPath));
+            }
+
+            if (bundleLabels == null)
+                throw new ArgumentNullException(nameof(bundleLabels));
+
+            if (string.IsNullOrWhiteSpace(filteredExtension))
+            {
+                throw new ArgumentException(
+                    "Filtered extension cannot be empty.",
+                    nameof(filteredExtension));
+            }
+
+            var result = new ContentsInfoList();
+            var directory = new DirectoryInfo(contentsRootDirectoryPath);
+            var extension = filteredExtension.TrimStart('.');
+            var files = directory.GetFiles($"*.{extension}");
+
+            foreach (var file in files)
+            {
+                var key = System.IO.Path
+                    .GetFileNameWithoutExtension(file.Name)
+                    .Replace('~', '/');
+                var label = string.Empty;
+
+                foreach (var pair in bundleLabels)
+                {
+                    if (!pair.Key.EndsWith(key, StringComparison.Ordinal))
+                        continue;
+
+                    label = pair.Value;
+                    break;
+                }
+
+                result.FileInfos.Add(new ContentsInfo
+                {
+                    Label = label,
+                    Path = file.Name,
+                    Hash = AssetBundleUtil.GetFileHash(file.FullName),
+                    Size = file.Length
+                });
+            }
+
+            result.FileCount = result.FileInfos.Count;
+            return result;
+        }
+    }
 }
